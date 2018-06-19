f03e74c094ea720a49ef656a2930159d1f10c5a1
BIG_whale <f411024@gmail.com>
2018-06-19 22:28:57 +0800

    add HW2

--- main.py
+++ main.py
@@ -6,6 +6,97 @@ import tabulate
 import nltk
 import time
 
+
+def chronology():
+    DataPath = r'meta/{0}-clean.txt'.format(fileName)
+    print("[人物事蹟年表]")
+
+    def descriptionFind(name):
+        # 搜尋人物所在的段落
+        rf = open(DataPath, 'r', encoding='utf8')
+        nameFinded = False
+        for line in rf.readlines():
+            m = re.match(name, line)
+            if(m is not None):
+                nameFinded = True
+            elif(nameFinded is True):
+                chronologyList(line)
+                return
+        print("查無此人")
+
+    def chronologyList(description):
+        # 建立年表
+        pat = r'\uFF08\d{4}[^\u3002]*'
+        yearList = re.findall(pat, description)
+        for line in yearList:
+            year = line[1:5]
+            workpat = r'[^\uFF0C].*'
+            nameDescription = re.search(workpat, line[6:]).group()
+            print("%s年 : %s" % (year, nameDescription))
+
+    # 主要程式
+    name = input("請輸入人名:")
+    descriptionFind(name)
+
+
+def the_number_of_verb():
+    """
+    計算數據
+    計算動詞數量並排序和列表
+    """
+    verbs = list()
+    # 取出所有動詞
+    for x in ws_text_tuple:
+        for v, pos in x:
+            if 'V' in pos:
+                verbs.append(v)
+    # 計算各動詞數量
+    fdist = nltk.FreqDist(verbs)
+    #
+    count = int(input('輸入 最頻繁x個的動詞的頻率(x 為一數量)\n'))
+    print(tabulate.tabulate([[x, y] for x, y in fdist.most_common(count)], [
+          '內容', '數量'], tablefmt='pipe'))
+
+
+def name_show_up():
+    """
+    計算某名字在該人物段落裡出現的次數
+    """
+
+
+def wordType_and_wordToken():
+    """
+    計算數據
+    計算各人物 word token 和 word type 數量
+    """
+    num_word_token = list()
+    num_word_type = list()
+    for x in ws_s_text:
+        num_word_token.append(len(x))
+        num_word_type.append(len(set(x)))
+
+    tabulate.WIDE_CHARS_MODE = True
+    count = int(input('輸入要幾筆資料\n'))
+    print(tabulate.tabulate([[name[x], num_word_token[x], num_word_type[x]] for x in range(0, count)], [
+          '內容', 'Token', 'Type'], tablefmt='pipe'))
+
+
+def n_gram():
+    """
+    計算數據
+    計算N-gram數量排行
+    """
+    n_gram = input('input n-grams 的 n 要多少?\n')
+    x = input('要前幾筆的排行\n')
+    fdist = nltk.FreqDist(
+        [word for people_based_ws_text in ws_s_text for word in people_based_ws_text if len(word) == int(n_gram)])
+    print(tabulate.tabulate(
+        [[w[0], w[1]] for w in fdist.most_common(int(x))],
+        ['內容', '數量'],
+        tablefmt='pipe'
+    ))
+
+
 if __name__ == '__main__':
     select = input(
         "你要分析哪篇文章呢？\n1. 社會與文化篇(005-362)  2. 政治與經濟篇(005-370)\n請輸入數字 1 or 2\n")
@@ -70,7 +161,7 @@ if __name__ == '__main__':
 
     # 存放清理及分檔後的結果進txt
     text = str()
-    with open('{0}-clean.txt'.format(fileName), encoding='utf-8', mode='w') as file:
+    with open('meta/{0}-clean.txt'.format(fileName), encoding='utf-8', mode='w') as file:
         for x in range(0, len(name)):
             text += name[x] + "\n"
             text += s_text[x] + "\n\n"
@@ -83,10 +174,10 @@ if __name__ == '__main__':
     '''
     ws_s_text = list()  # 純文字結果  ex ['蔡英文', '是', ...]
     ws_text_tuple = list()  # 含詞性結果 ex [('蔡英文', 'Nb'), ('是', 'SHI'), ...]
-    if (not os.path.isfile('{0}.json'.format(fileName))) or (os.path.getsize('{0}.json'.format(fileName)) == 0):
+    if (not os.path.isfile('meta/{0}.json'.format(fileName))) or (os.path.getsize('meta/{0}.json'.format(fileName)) == 0):
         print('info :進行分詞中')
         t_start = time.clock()
-        with open('{0}.json'.format(fileName), encoding='utf-8', mode='w') as jsonFile:
+        with open('meta/{0}.json'.format(fileName), encoding='utf-8', mode='w') as jsonFile:
             WS = ckipws.CKIPWS()  # 呼叫中研院分詞程式
             for i, t in enumerate(s_text):
                 tmp = list()
@@ -101,7 +192,7 @@ if __name__ == '__main__':
         print('info :完成分詞 花了: {0}秒'.format(time.clock() - t_start))
     else:
         print('info :讀取舊有已分詞過之檔案')
-        with open('{0}.json'.format(fileName), encoding='utf-8', mode='r') as jsonFile:
+        with open('meta/{0}.json'.format(fileName), encoding='utf-8', mode='r') as jsonFile:
             ws_text_tuple = json.load(jsonFile)
             for x in ws_text_tuple:
                 tmp = list()
@@ -112,33 +203,32 @@ if __name__ == '__main__':
     """
     存放分詞後結果進文字檔
     """
-    with open('{0}-ws.txt'.format(fileName), encoding='utf-8', mode='w') as wsFile:
+    with open('meta/{0}-ws.txt'.format(fileName), encoding='utf-8', mode='w') as wsFile:
         text = str()
         for x in range(0, len(name)):
             text += name[x] + "\n"
-            text += str(ws_s_text[x]) + "\n\n"
+            text += str(ws_text_tuple[x]) + "\n\n"
         wsFile.write(text)
 
     """
-    計算相關數據
+    選擇要分析的內容
     """
-    num_word_token = list()
-    num_word_type = list()
-    for x in ws_s_text:
-        num_word_token.append(len(x))
-        num_word_type.append(len(set(x)))
-
-    tabulate.WIDE_CHARS_MODE = True
-    print('\n因為console寬度的關系 只展示前10筆資料')
-    print(tabulate.tabulate([num_word_token[:10],
-                             num_word_type[:10]], name[:10], tablefmt='pipe'))
-
-    n_gram = input('input n-grams 的 n 要多少?\n')
-    x = input('要前幾筆的排行\n')
-    fdist = nltk.FreqDist(
-        [word for people_based_ws_text in ws_s_text for word in people_based_ws_text if len(word) == int(n_gram)])
-    print(tabulate.tabulate(
-        [[x[1] for x in fdist.most_common(int(x))]],
-        [x[0] for x in fdist.most_common(int(x))],
-        tablefmt='pipe'
-    ))
+    choice = input(
+        "輸入想要計算的內容(數字)\n" +
+        "(1)計算各人物 word token 和 word type 數量\n" +
+        "(2)計算N-gram數量排行\n" +
+        "(3)計算動詞數量並排序和列表\n" +
+        "(4)計算某名字在該人物段落裡出現的次數 \n" +
+        "(5)人物事蹟年表\n")
+    if choice == '1':
+        wordType_and_wordToken()
+    elif choice == '2':
+        n_gram()
+    elif choice == '3':
+        the_number_of_verb()
+    elif choice == '4':
+        name_show_up()
+    elif choice == '5':
+        chronology()
+    else:
+        print("Without this option")
